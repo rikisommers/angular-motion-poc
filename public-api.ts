0a6710ccecd414a0b9c6619f59f4fe99efdb1558
rikisommers <rikisommers@yahoo.co.nz>
2025-10-19 22:35:09 +1300

    trying to publish anything

--- public-api.ts
+++ public-api.ts
@@ -4,6 +4,7 @@
 
 // Export all directives
 export { MotionOneDirective } from './src/app/directives/motion-one.directive';
+export { MotionIfDirective } from './src/app/directives/motion-if.directive';
 
 // Export interfaces and types
 export type{
@@ -13,6 +14,6 @@ export type{
 } from './src/app/directives/motion-one.directive';
 
 // Export services
-// export { MotionOneService } from './src/app/services/motion-one.service';
-// export { MotionAnimationService } from './src/app/services/motion-animation.service';
-// export { MotionService } from './src/app/services/motion.service';
+export { MotionOneService } from './src/app/services/motion-one.service';
+export { MotionAnimationService } from './src/app/services/motion-animation.service';
+export { MotionService } from './src/app/services/motion.service';
