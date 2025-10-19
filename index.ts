d16f35236e0e7d041e9dcb21b31e4d123b4107f1
rikisommers <rikisommers@yahoo.co.nz>
2025-10-19 22:01:55 +1300

    update package outputs

---
+++ index.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of @rs-components/ng-motion
+ */
+
+export * from './public-api';
---
+++ public-api.ts
@@ -0,0 +1,20 @@
+/*
+ * Public API Surface of @rs-components/ng-motion
+ */
+
+// Export all directives
+export { MotionOneDirective } from './src/app/directives/motion-one.directive';
+export { MotionIfDirective } from './src/app/directives/motion-if.directive';
+export { MotionDirective } from './src/directives/ngx-motion.directive';
+
+// Export interfaces and types
+export type{
+  TransitionOptions,
+  VariantWithTransition,
+  TimelineStep
+} from './src/app/directives/motion-one.directive';
+
+// Export services
+export { MotionOneService } from './src/app/services/motion-one.service';
+export { MotionAnimationService } from './src/app/services/motion-animation.service';
+export { MotionService } from './src/app/services/motion.service';
