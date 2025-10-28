9b093d5510dd29a877527103a2d29772cbd729c3
rikisommers <rikisommers@yahoo.co.nz>
2025-10-28 08:31:15 +0000

    Deploying to main from @ rikisommers/angular-motion-poc@3d9fb46b7edb259c92d7c2239661d308c156e423 🚀

--- docs/ngx-motion/public-api.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/directives/motion-one.directive';
-export * from './lib/directives/motion-if.directive';
-export * from './lib/services/motion-one.service';
-export * from './lib/services/motion-animation.service';
-export type { TransitionOptions, VariantWithTransition, TimelineStep } from './lib/directives/motion-one.directive';
-export type { MotionElement } from './lib/services/motion-one.service';
